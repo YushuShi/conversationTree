c02f00f6945c0f82798da86927a53800a03e48c6
Yushu Shi <36617896+YushuShi@users.noreply.github.com>
2026-01-09 18:45:10 -0500

    changed start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
-PORT_VALUE="${PORT:-8080}"
+PORT_VALUE="${PORT:-${RAILWAY_PORT:-8080}}"
+echo "Starting reflex on port ${PORT_VALUE}"
 
 exec reflex run --env prod --single-port --backend-host 0.0.0.0 --frontend-port "$PORT_VALUE"
